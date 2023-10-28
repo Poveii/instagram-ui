7f35da07117f54625c845b2fa9c6339fa0788c8a
Poveii <yellowtutors@gmail.com>
2023-10-28 15:37:14 -0300

    chore: Color Palette and Gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        gradient:
+          'radial-gradient(var(--first-gradient)), radial-gradient(var(--second-gradient))',
+      },
+      colors: {
+        gray: {
+          200: '#C7C7C7',
+          400: '#8E8E8E',
+          500: '#737373',
+          700: '#262626',
+        },
+        blue: '#0095F6',
+        red: '#FF3040',
+      },
+    },
   },
   plugins: [],
 }
