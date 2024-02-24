22d47e0389989cbd83b35763350c2ab6c8a317c5
Poveii <yellowtutors@gmail.com>
2024-02-24 18:29:03 -0300

    refactor: Change way to use `tailwindcss` instead `useState`

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,5 +1,4 @@
 import { Icon } from '@phosphor-icons/react'
-import { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 interface IMenuItemProps {
@@ -18,22 +17,32 @@ export function MenuItem({
   link,
   bigger = false,
 }: IMenuItemProps) {
-  const [active, setActive] = useState(false)
-
-  const MenuItemStyles =
-    'flex gap-5 font-bold text-xl items-center hover:opacity-70 hover:scale-[1.03]' +
-    (bigger ? ' py-4' : '')
-
   return (
     <li>
       <NavLink
         to={link}
-        className={({ isActive }) => {
-          isActive ? setActive(true) : setActive(false)
-          return MenuItemStyles
-        }}
+        className={({ isActive }) =>
+          'flex gap-5 font-bold text-xl items-center hover:opacity-70 hover:scale-[1.03] group' +
+          (bigger ? ' py-4' : '') +
+          (isActive ? ' active' : '')
+        }
       >
-        {Icon ? <Icon size={32} weight={active ? 'fill' : 'regular'} /> : <></>}
+        {Icon ? (
+          <>
+            <Icon
+              size={32}
+              weight="fill"
+              className="hidden peer group-[.active]:block"
+            />
+            <Icon
+              size={32}
+              weight="regular"
+              className="group-[.active]:hidden"
+            />
+          </>
+        ) : (
+          <></>
+        )}
         {photo ? (
           <img
             src={photo}
